9ec04dae23a53d1fb3f422b03fbc906db1177c77
Ramsey Dow <yesmar@gmail.com>
2018-08-11 15:59:30 -0700

    Add rm_replace API to replace one res with another
    Fixes #2

--- code/rm.h
+++ code/rm.h
@@ -7,6 +7,7 @@ typedef void (*release_res)(void *res);
 
 rm_handle *rm_alloc(size_t capacity, size_t growth_factor);
 int rm_push(rm_handle *rm, void *res, release_res free_res);
+void *rm_replace(rm_handle *rm, void *res, void *new_res);
 void rm_free(rm_handle **rm);
 #define rm_ret(rm) do { rm_free(&(rm)); return; } while(0)
 #define rm_ret_code(rm, ret) do { rm_free(&(rm)); return ret; } while(0)
